24dade7d0ad3a4c8e781b373e4793cb7e18879be
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-02-28 11:15:53 -0800

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,7 +22,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		nnode->left = parent->left;
 		parent->left->parent = nnode;
 	}
-		parent->left = nnode;
-		
-		return (nnode);
+	parent->left = nnode;
+	return (nnode);
 }
