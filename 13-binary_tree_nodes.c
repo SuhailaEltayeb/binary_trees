94759b202e61c07b28c840b2d147865de4d97fdb
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-02-29 11:46:26 -0800

    13-binary_tree_nodes.c

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - function to calculate nodes with 1 child in BT
+ * @tree: pointer to root node of tree to calculate its nodes
+ *
+ * Return: if successful, # on nodes, otherwise 0
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	size_t leave_nodes = 0;
+
+	if (tree->right || tree->left)
+		leave_nodes = leave_nodes + 1;
+	leave_nodes = leave_nodes + binary_tree_nodes(tree->right);
+	leave_nodes = leave_nodes + binary_tree_nodes(tree->left);
+	return (leave_nodes);
+}
