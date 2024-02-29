4178958af3bc4f7e6c90028e0d4e8966af2843c0
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-02-29 10:57:49 -0800

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,10 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	size_t L_subtree = binary_tree_height(tree->left);
-	size_t R_subtree = binary_tree_height(tree->right);
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-	return (1 + L_subtree > R_subtree ? L_subtree : R_subtree);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
