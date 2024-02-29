7d869c3eb99faaaf516048c4e375921b7b0543e9
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-02-29 10:06:29 -0800

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_height - function to calculate tree height
- * @tree: pointer to root node of tree to which height is measured
+ * @tree: pointer to root node of tree to which height is measureid
  * Return: if successful, tree height, 0 otherwise
  */
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -12,8 +12,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t R_height =  binary_tree_height(tree->right);
 	size_t L_height =  binary_tree_height(tree->left);
 
-	if (R_height > L_height)
-		return (1 + R_height);
-	else
-		return (1 + L_height);
+	return (1 + (R_height > L_height ? R_height : L_height));
 }
