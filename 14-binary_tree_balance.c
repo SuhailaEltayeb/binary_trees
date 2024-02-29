04d67448e82d74d4abc72aa43509c0fdf4fe2457
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-02-29 15:28:32 -0800

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -31,10 +31,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	size_t L_height = binary_tree_height(tree->left);
-	size_t R_height = binary_tree_height(tree->right);
-
-	return (int)(L_height - R_height);
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (0);
 }
