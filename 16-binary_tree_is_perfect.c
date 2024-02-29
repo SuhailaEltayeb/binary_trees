a0d896f39cacf356fb5dab3e22c51976adc143f1
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-02-29 14:20:31 -0800

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,22 +11,24 @@ size_t binary_tree_size(const binary_tree_t *tree);
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    int height = 0, nodes = 0, expected_nodes = 1;
+	int height = 0, actual_nodes = 0, P_nodes_count = 1;
 
-    if (tree == NULL)
-        return 0;
+	if (tree == NULL)
+		return (0);
 
-    height = binary_tree_height(tree);
-    nodes = binary_tree_size(tree);
+	height = binary_tree_height(tree);
+	actual_nodes = binary_tree_size(tree);
 
-    while (height > 0)
-    {
-        expected_nodes <<= 1;
-        expected_nodes++;
-        height--;
-    }
-
-    return (nodes == expected_nodes);
+	while (height > 0)
+	{
+		P_nodes_count <<= 1;
+		P_nodes_count++;
+		height--;
+	}
+	if (actual_nodes == P_nodes_count)
+		return (1);
+	else
+		return (0);
 }
 
 /**
@@ -36,23 +38,22 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree)
-    {
-        size_t R_subtree = 0, L_subtree = 0;
+	if (tree)
+	{
+		size_t R_subtree = 0, L_subtree = 0;
 
-        if (tree->left != NULL)
-        {
-            L_subtree = 1 + binary_tree_height(tree->left);
-        }
-        if (tree->right != NULL)
-        {
-            R_subtree = 1 + binary_tree_height(tree->right);
-        }
-        return ((L_subtree > R_subtree) ? L_subtree : R_subtree);
-    }
-    return (0);
+		if (tree->left != NULL)
+		{
+			L_subtree = 1 + binary_tree_height(tree->left);
+		}
+		if (tree->right != NULL)
+		{
+			R_subtree = 1 + binary_tree_height(tree->right);
+		}
+		return ((L_subtree > R_subtree) ? L_subtree : R_subtree);
+	}
+	return (0);
 }
-
 /**
  * binary_tree_size - function to calculate the size of a BT
  * @tree: pointer to root node of tree to which size is measured
@@ -61,7 +62,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    return (1 + binary_tree_size(tree->right) + binary_tree_size(tree->left));
+	if (tree == NULL)
+		return (0);
+	return (1 + binary_tree_size(tree->right) + binary_tree_size(tree->left));
 }
