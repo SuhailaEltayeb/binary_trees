e1d269805b351af0ac86b476dfc0e9e260b96e78
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-02-29 13:13:00 -0800

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,9 +10,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if ((tree->right == NULL && tree->left == NULL) ||
-	(tree->right && tree->left))
-		return (binary_tree_is_full(tree->right) &&
-			binary_tree_is_full(tree->left));
+	if (tree->right == NULL && tree->left == NULL)
+		return (1);
+	if (tree->right != NULL && tree->left != NULL)
+		return binary_tree_is_full(tree->right) &&
+			binary_tree_is_full(tree->left);
 	return (0);
 }
