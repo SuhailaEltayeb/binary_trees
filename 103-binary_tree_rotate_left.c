eb76b20d5135ae62fb120bbcae9def4aee994522
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-02-29 17:20:50 -0800

    103-binary_tree_rotate_left.c

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,29 +8,25 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    if (tree == NULL || tree->right == NULL)
+	binary_tree_t *right_child = tree->right;
+	binary_tree_t *right_left_child = right_child->left;
+	
+	if (tree == NULL || tree->right == NULL)
         return NULL;
-
-    binary_tree_t *right_child = tree->right;
-    binary_tree_t *right_left_child = right_child->left;
-
-    right_child->left = tree;
-    tree->right = right_left_child;
-
-    if (right_left_child != NULL)
-        right_left_child->parent = tree;
-
-    binary_tree_t *parent = tree->parent;
-    tree->parent = right_child;
-    right_child->parent = parent;
-
-    if (parent != NULL)
-    {
-        if (parent->left == tree)
-            parent->left = right_child;
-        else
-            parent->right = right_child;
-    }
-
-    return right_child;
+	
+	right_child->left = tree;
+	tree->right = right_left_child;
+	if (right_left_child != NULL)
+	       	right_left_child->parent = tree;
+	binary_tree_t *parent = tree->parent;
+	tree->parent = right_child;
+	right_child->parent = parent;
+	if (parent != NULL)
+	{
+		if (parent->left == tree)
+			parent->left = right_child;
+		else
+			parent->right = right_child;
+	}
+	return right_child;
 }
