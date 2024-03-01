e005cbbd7790cb8523b46ea69d699dce4987d985
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-02-29 16:22:14 -0800

    104-binary_tree_rotate_right.c

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -10,21 +10,23 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
 	if (tree == NULL || tree->left == NULL)
 		return (NULL);
-	binary_tree_t *new_root = tree->left;
-	binary_tree_t *right_subtree = new_root->right;
+	binary_tree_t *new_root, *right_subtree;
 
+	new_root = tree->left;
+	right_subtree = new_root->right;
+	new_root->right = tree;
 	tree->left = right_subtree;
 	if (right_subtree != NULL)
 		right_subtree->parent = tree;
-	new_root->right = tree;
-	new_root->parent = tree->parent;
+	right_subtree = tree->parent;
 	tree->parent = new_root;
-	if (new_root->parent != NULL)
+	new_root->parent = right_subtree;
+	if (right_subtree != NULL)
 	{
-		if (new_root->parent->left == tree)
-			new_root->parent->left = new_root;
+		if (right_subtree->left == tree)
+			right_subtree->left = new_root;
 		else
-			new_root->parent->right = new_root;
+			right_subtree->right = new_root;
 	}
 	return (new_root);
 }
